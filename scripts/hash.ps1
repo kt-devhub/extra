dd7aa0bef95a9142078a8aa8bf44080f85da56ef
KT <kt-devhub@outlook.com>
2025-02-28 00:31:07 +0800

    created resizer

--- scripts/hash.ps1
+++ scripts/hash.ps1
@@ -1,7 +1,7 @@
 # Define the URL and the local file paths
-$url = "https://c.1password.com/dist/1P/win8/1PasswordSetup-8.10.66-8.NIGHTLY.exe"
-$originalPath = "D:\Downloads\1PasswordSetup-8.10.66-8.NIGHTLY.exe"
-$renamedPath = "D:\Downloads\dl.7z"
+$url = "https://download.resize-c.com/resizer-free-portable.zip"
+$originalPath = "D:\Downloads\resizer-free-portable.zip"
+# $renamedPath = "D:\Downloads\dl.7z"
 
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $originalPath
@@ -12,13 +12,13 @@ while (-not (Test-Path $originalPath)) {
 }
 
 # Copy and rename the file
-Copy-Item -Path $originalPath -Destination $renamedPath
+# Copy-Item -Path $originalPath -Destination $renamedPath
 
 # Generate the hash for the original file (SHA256 in this example)
 $originalHash = Get-FileHash -Path $originalPath -Algorithm SHA256
 Write-Output "Original File Hash: $($originalHash.Hash)"
 
 # Generate the hash for the renamed file (SHA256 in this example)
-$renamedHash = Get-FileHash -Path $renamedPath -Algorithm SHA256
-Write-Output "Renamed File Hash: $($renamedHash.Hash)"
+# $renamedHash = Get-FileHash -Path $renamedPath -Algorithm SHA256
+#  Write-Output "Renamed File Hash: $($renamedHash.Hash)"
 
